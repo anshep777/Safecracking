9c5229fee520ddfa39f6ffa178bc71dd50eaadd8
DesignFreak2 <37663241+DesignFreak2@users.noreply.github.com>
2018-12-11 09:36:34 -0600

    Add files via upload

---
+++ DualServo.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+Servo servoOne; //Handle puller servo
+Servo servoTwo; //Handle pull back servo
+
+int positionOne = 0; //servoOne's position
+int positionTwo = 0; //servoTwo's position
+
+void setup(){
+  servoOne.attach(9);
+  servoTwo.attach(10);
+}
+
+void loop(){
+  servoOne.write(90);
+  delay(500);
+  servoOne.write(0);
+  servoTwo.write(90);
+  delay(500);
+  servoTwo.write(0);
+  delay(500);
+}
+
