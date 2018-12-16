ad3f4dd64d8119249472001a23b9afa1e26afe15
DesignFreak2 <37663241+DesignFreak2@users.noreply.github.com>
2018-12-15 20:27:41 -0600

    Add files via upload

---
+++ peltierTesting.ino
@@ -0,0 +1,37 @@
+// Adafruit Motor shield library
+// copyright Adafruit Industries LLC, 2009
+// this code is public domain, enjoy!
+
+#include <AFMotor.h>
+
+
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Motor test!");
+}
+
+void loop() {
+
+  motor2.setSpeed(150);
+  motor3.setSpeed(150);
+
+  
+ 
+  motor2.run(FORWARD); 
+  motor3.run(FORWARD);  
+  delay(10000);
+
+
+  motor2.setSpeed(150);
+  motor3.setSpeed(150);
+
+  motor2.run(BACKWARD); 
+  motor3.run(BACKWARD); 
+  
+  delay(10000);
+
+}
